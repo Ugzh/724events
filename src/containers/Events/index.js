411f087bf284daae45099317d0205d0a59c2980d
Ugzher <ugo.mateda@gmail.com>
2024-08-26 16:13:03 +0200

    Select statement

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState} from "react";
 import EventCard from "../../components/EventCard";
 import Select from "../../components/Select";
 import { useData } from "../../contexts/DataContext";
@@ -13,63 +13,61 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
+  const filteredEvents = (!type
       ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
+      : data?.events.filter(event => event.type === type)) || [];
+
+  const eventPage = filteredEvents.filter((event, index) =>
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+  );
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+  const pageNumber = Math.ceil((filteredEvents?.length || 0) / PER_PAGE);
   const typeList = new Set(data?.events.map((event) => event.type));
+
+
   return (
-    <>
-      {error && <div>An error occured</div>}
-      {data === null ? (
-        "loading"
-      ) : (
-        <>
-          <h3 className="SelectTitle">Catégories</h3>
-          <Select
-            selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
-          />
-          <div id="events" className="ListContainer">
-            {filteredEvents.map((event) => (
-              <Modal key={event.id} Content={<ModalEvent event={event} />}>
-                {({ setIsOpened }) => (
-                  <EventCard
-                    onClick={() => setIsOpened(true)}
-                    imageSrc={event.cover}
-                    title={event.title}
-                    date={new Date(event.date)}
-                    label={event.type}
-                  />
-                )}
-              </Modal>
-            ))}
-          </div>
-          <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
-                {n + 1}
-              </a>
-            ))}
-          </div>
-        </>
-      )}
-    </>
+      <>
+        {error && <div>An error occured</div>}
+        {data === null ? (
+            "loading"
+        ) : (
+            <>
+              <h3 className="SelectTitle">Catégories</h3>
+              <Select
+                  selection={Array.from(typeList)}
+                  onChange={(value) => (value ? changeType(value) : changeType(null))}
+              />
+              <div id="events" className="ListContainer">
+                {eventPage.map((event) => (
+                    <Modal key={event.id} Content={<ModalEvent event={event} />}>
+                      {({ setIsOpened }) => (
+                          <EventCard
+                              onClick={() => setIsOpened(true)}
+                              imageSrc={event.cover}
+                              title={event.title}
+                              date={new Date(event.date)}
+                              label={event.type}
+                          />
+                      )}
+                    </Modal>
+                ))}
+              </div>
+              <div className="Pagination">
+                {[...Array(pageNumber || 0)].map((_, n) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+                      {n + 1}
+                    </a>
+                ))}
+              </div>
+            </>
+        )}
+      </>
   );
 };
 
